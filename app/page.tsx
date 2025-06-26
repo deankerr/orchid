6e0302ec8a930d8da4073b5aeae1638765ef839d
Dean Kerr <dean.kerr@gmail.com>
2025-06-26 18:55:49 +1000

    wrap home page in Suspense for nuqs

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,12 @@
-'use client'
+import { Suspense } from 'react'
 
-import { parseAsString, useQueryState } from 'nuqs'
-
-import { AppLayout } from '@/components/app-layout'
+import { HomePage } from '@/components/home-page'
 import { DataStreamLoader } from '@/components/loading'
-import { ModelList } from '@/components/model-list'
-import { ModelPage } from '@/components/model-page'
-import { PageContainer } from '@/components/page-container'
-import { useFilteredModels } from '@/hooks/use-filtered-models'
-import { useKeypress } from '@/hooks/use-keypress'
 
 export default function Home() {
-  const [modelSlug, setModelSlug] = useQueryState('model', parseAsString)
-  const filteredModels = useFilteredModels()
-
-  // Keyboard navigation for model pages
-  useKeypress(
-    modelSlug && filteredModels
-      ? {
-          ArrowRight: () => {
-            const currentIndex = filteredModels.findIndex((model) => model.slug === modelSlug)
-            if (currentIndex !== -1) {
-              const nextIndex = (currentIndex + 1) % filteredModels.length
-              setModelSlug(filteredModels[nextIndex].slug)
-            }
-          },
-          ArrowLeft: () => {
-            const currentIndex = filteredModels.findIndex((model) => model.slug === modelSlug)
-            if (currentIndex !== -1) {
-              const prevIndex = (currentIndex - 1 + filteredModels.length) % filteredModels.length
-              setModelSlug(filteredModels[prevIndex].slug)
-            }
-          },
-        }
-      : {},
-  )
-
   return (
-    <AppLayout>
-      <PageContainer>
-        {modelSlug ? (
-          <ModelPage slug={modelSlug} />
-        ) : filteredModels ? (
-          <ModelList models={filteredModels} />
-        ) : (
-          <DataStreamLoader label="Loading models..." />
-        )}
-      </PageContainer>
-    </AppLayout>
+    <Suspense fallback={<DataStreamLoader label="Loading..." />}>
+      <HomePage />
+    </Suspense>
   )
 }
