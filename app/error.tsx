edb16688e7572f97378bf6b57639a4c4267645c7
Dean Kerr <dean.kerr@gmail.com>
2025-07-24 02:09:59 +1000

    add app/error.tsx

---
+++ app/error.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { XCircle } from 'lucide-react'
+
+import { PageContainer } from '@/components/shared/page-container'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { Button } from '@/components/ui/button'
+
+export default function ErrorPage({ error }: { error: Error & { digest?: string } }) {
+  const isDev = process.env.NODE_ENV === 'development'
+
+  return (
+    <PageContainer className="flex flex-col items-center justify-center">
+      <Alert variant="destructive">
+        <XCircle />
+        <AlertTitle>Something went wrong</AlertTitle>
+        <AlertDescription>
+          An error occurred while rendering this page. These options may help:
+          <div className="mt-2 space-x-2">
+            <Button variant="secondary" onClick={() => window.location.reload()}>
+              Refresh Page
+            </Button>
+
+            <Button variant="secondary" onClick={() => (window.location.href = '/')}>
+              Go Home
+            </Button>
+          </div>
+          {error?.message && isDev && <div className="py-3 font-mono text-sm">{error.message}</div>}
+          {error?.digest && <div className="py-3 font-mono text-xs">Digest: {error.digest}</div>}
+        </AlertDescription>
+      </Alert>
+    </PageContainer>
+  )
+}
