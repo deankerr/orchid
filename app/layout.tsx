220690bfce62c2aa870b22aeb5e52016c5a40609
Dean Kerr <dean.kerr@gmail.com>
2025-07-24 03:20:22 +1000

    add vercel analytics/speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 
 import { ThemeProvider } from '@/app/theme-provider'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 
 import { AppLayout } from './app-layout'
@@ -44,6 +46,8 @@ export default function RootLayout({
             </ThemeProvider>
           </ConvexClientProvider>
         </NuqsAdapter>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
