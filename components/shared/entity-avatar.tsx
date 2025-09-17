96e25c632479ad563f1cdad33fb036beb40ad759
Dean Kerr <dean.kerr@gmail.com>
2025-09-17 14:10:58 +1000

    use static resized model/provider icons

--- components/shared/entity-avatar.tsx
+++ components/shared/entity-avatar.tsx
@@ -1,5 +1,3 @@
-import { getImageProps } from 'next/image'
-
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { cn } from '@/lib/utils'
 
@@ -12,16 +10,9 @@ export function EntityAvatar({
   src: string
   fallback: string
 } & React.ComponentProps<typeof Avatar>) {
-  const { props: imageProps } = getImageProps({
-    src,
-    alt: '', // adjacent to entity name
-    width: 24,
-    height: 24,
-  })
-
   return (
     <Avatar className={cn('size-6 shrink-0 rounded-sm', className)} {...props}>
-      <AvatarImage {...imageProps} />
+      {src && <AvatarImage src={src} />}
       <AvatarFallback className="rounded-sm font-mono text-sm uppercase">
         {fallback.replace(/[^a-zA-Z0-9]/g, '').slice(0, 2)}
       </AvatarFallback>
