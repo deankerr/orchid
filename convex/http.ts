c9c9a685bb22e54999a344997e6a3138b5405f5c
Dean Kerr <dean.kerr@gmail.com>
2025-11-19 09:28:34 +1100

    restore http /bundle endpoint

--- convex/http.ts
+++ convex/http.ts
@@ -4,8 +4,35 @@ import * as R from 'remeda'
 import { api } from './_generated/api'
 import { httpAction } from './_generated/server'
 import { bundleSyncHttpHandler } from './admin/bundleSync'
+import { getArchiveBundle } from './snapshots/shared/bundle'
 
 const http = httpRouter()
+
+http.route({
+  path: '/bundle',
+  method: 'GET',
+  handler: httpAction(async (ctx, req) => {
+    const url = new URL(req.url)
+    const crawlId = url.searchParams.get('crawl_id')
+
+    if (!crawlId) {
+      return new Response('Missing crawl_id parameter', { status: 400 })
+    }
+
+    const bundle = await getArchiveBundle(ctx, crawlId)
+
+    if (!bundle) {
+      return new Response('Bundle not found', { status: 404 })
+    }
+
+    return new Response(JSON.stringify(bundle), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  }),
+})
 
 // Power user API (preview)
 http.route({
